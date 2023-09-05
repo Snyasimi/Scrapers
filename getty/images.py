a6e5aa2f343d1207ada7d393971d6ccdee6b4ab3
Snyasimi <nyasimisolo@gmail.com>
2023-09-05 12:35:45 +0300

    initialization

---
+++ getty/images.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.gettyimages.com/search/2/image?family=creative&phrase=cow"
+keyword = input('Input image keyword:\n')
+
+url = f"https://www.gettyimages.com/search/2/image?family=creative&phrase={keyword}"
+headers = {
+ 'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0'
+}
+
+page = requests.get(url,headers=headers)
+
+try:
+ if page.status_code == 200:
+  # print(page.text)
+  soup = BeautifulSoup(page.text, 'html.parser')
+  figures = soup.find_all('figure', limit=1)
+
+  for figure in figures:
+   images = figure.img
+   tags = images.attrs
+   image_src = tags['src']
+   desc = tags['alt']
+   print(f"IMAGE SRC : {image_src}\nIMAGE ALT : {desc}")
+except e:
+ print(f"There was an error {e}")
+
